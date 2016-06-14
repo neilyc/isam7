ee900f4028688a8b06ad50da6145322b5803072e
unknown <flogarinho@odity.fr>
2016-06-14 17:50:04 +0300

    -bootstrap

--- web/js/app/app.js
+++ web/js/app/app.js
@@ -1,22 +1,29 @@
 ;(function(window) {
   'use strict';
+  var self;
   function App() {
-    this._init();
+    self = this;
+    self._init();
   };
   App.prototype = {
-    _init : function() {   
-      window.onload = function() {
-        document.body.removeChild(document.querySelector('.preloader'));
-      }
+    _init: function() {   
+      document.querySelector('.toggle-nav').addEventListener('click', function(e) {
+        document.querySelector('.topnav').classList.toggle("responsive");
+      });
 
       if(document.querySelector('.gallery')) {
-        this._initGallery();
+        self._initGallery();
       }
       if(document.querySelector('.blog')) {
-        this._initBlog();
+        self._initBlog();
+      }
+
+      window.onload = function() {
+        self._menuCheckActive();
+        document.body.removeChild(document.querySelector('.preloader'));
       }
     },
-    _initGallery : function() {
+    _initGallery: function() {
       new AnimOnScroll(document.getElementById('grid'), {
         minDuration : 0.4,
         maxDuration : 0.7,
@@ -25,7 +32,7 @@
 
       new Modal('.img');
     },
-    _initBlog : function() {
+    _initBlog: function() {
       new Swiper('.swiper-container', {
         slidesPerView: 4,
         paginationClickable: true,
@@ -36,6 +43,18 @@
       });
 
       new Modal('.img');
+    },
+    _menuCheckActive: function() {
+      var urlMatch = false,
+          path = decodeURIComponent(window.location.pathname),
+          href;
+
+      Array.prototype.forEach.call(document.querySelectorAll('.topnav li a'), function (elem) {
+        href = elem.getAttribute('href');
+        if (path.substring(0, href.length) === href && href != '/') {
+          elem.classList.add('current');
+        }
+      });
     }
   }
   window.App = App;
